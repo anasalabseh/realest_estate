47f647e2b70845368f92c0d4f5099ee9607e8179
Anas al-Absah <anas.abseh@gmail.com>
2024-08-30 23:09:37 +0300

    add listings app
    an app for dealing with listing in the real estate system which provides to the frontend the ability to retrieve the listings information and search for listings with the desired filters

---
+++ backend/listings/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils.timezone import now
+from realtors.models import Realtor
+
+def liting_main_image_path(instance, filename):
+    return f"photos/listing_{instance.title}/main/{filename}"
+
+def listing_gallery_image_path(instance, filename):
+    return f"photos/listing_{instance.title}/%Y/%m/%d/{filename}"
+
+class Listing(models.Model):
+    class SaleType(models.TextChoices):
+        FOR_SALE = 'For Sale'
+        FOR_RENT = 'For Rent'
+
+    class HomeType(models.TextChoices):
+        HOUSE = 'House'
+        CONDO = 'Condo'
+        TOWNHOUSE = 'Townhouse'
+
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    slug = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=150)
+    address = models.CharField(max_length=150)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=15)
+    description = models.TextField(blank=True)
+    sale_type = models.CharField(max_length=50, choices=SaleType.choices, default=SaleType.FOR_SALE)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    home_type = models.CharField(max_length=20, choices=HomeType.choices, default=HomeType.HOUSE)
+    sqft = models.IntegerField()
+    open_house = models.BooleanField(default=False)
+    photo_main = models.ImageField(upload_to=liting_main_image_path)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=now, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class LitingImage(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=listing_gallery_image_path)
---
+++ backend/listings/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ListingsView, ListingView, SearchView
+
+urlpatterns = [
+    path('', ListingsView.as_view(), name='listings_list'),
+    path('search', SearchView.as_view(), name='listings_search'),
+    path('<slug>', ListingView.as_view(), name='listing_details')
+]
